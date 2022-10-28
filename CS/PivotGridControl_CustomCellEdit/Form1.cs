3decf0396b8b747a36b1dfca171c4f32bc91a559
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2022-10-28 11:28:42 +0400

    Update Form1.cs

--- CS/PivotGridControl_CustomCellEdit/Form1.cs
+++ CS/PivotGridControl_CustomCellEdit/Form1.cs
@@ -9,34 +9,27 @@ namespace PivotGridControl_CustomCellEdit {
         public Form1() {
             InitializeComponent();
         }
-
         // Creates new repository items.
         RepositoryItemTextEdit riTextEdit = new RepositoryItemTextEdit();
         RepositoryItemProgressBar riProgressBar = new RepositoryItemProgressBar();
-
         private void Form1_Load(object sender, EventArgs e) {
             this.salesPersonTableAdapter.Fill(this.nwindDataSet.SalesPerson);
-
             // Initializes cell editors for this Pivot Grid control.
             pivotGridControl1.RepositoryItems.AddRange(new RepositoryItem[] { riTextEdit, riProgressBar });
         }
-
         void pivotGridControl1_CustomCellEdit(object sender, PivotCustomCellEditEventArgs e) {
             // Specifies a cell editor which is used in both display and edit modes.
             if (e.DataField == fieldQuantityPercent & e.RowValueType == PivotGridValueType.Value)
                     e.RepositoryItem = riProgressBar;            
         }
-
         void pivotGridControl1_CustomCellEditForEditing(object sender, PivotCustomCellEditEventArgs e) {
             // Overrides the cell editor for the edit mode.
             if (e.DataField == fieldQuantityPercent & e.RowValueType == PivotGridValueType.Value) 
                     e.RepositoryItem = riTextEdit;            
-        }
-        
+        }     
         private void pivotGridControl1_CustomCellValue(object sender, PivotCellValueEventArgs e) {
             if (e.DataField == fieldQuantityPercent)
                 e.Value = Math.Round(Convert.ToDecimal(e.Value) * 100, 2);
         }
-
     }
 }
